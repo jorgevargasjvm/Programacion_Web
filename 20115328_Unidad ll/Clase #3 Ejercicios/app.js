228228f8190d6effa4d67a0706a8e8d2cfac7358
Jorge Vargas <jorgevargasmateo@gmail.com>
2020-05-31 18:06:53 -0400

    UPDATE: Ejercicio complementario Triangulo

---
+++ 20115328_Unidad ll/Clase #3 Ejercicios/app.js
@@ -0,0 +1,4 @@
+document.write('<header><h1>Heading</h1></header>');
+document.write('<div class ="B"> <p>I never am really satisfied that I undestand anything: because, undestand it well as I may, my comprehension can only be an indefinitesimal fraction of all I want to undestand aboyt the many connections and relations which occur to me, how the matter in question was first thought of or arraived at. ETC,ETC  </p> </div>');
+document.write('<aside> <div class ="B2"> <p>In almost every computation <br> a great wariety <br> of arrancements for the <br> succession of the processes <br> is imposible and <br> various considerations <br> must influence the selections <br> amongst them from <br> the purposes of <br> a calculating engine</p> </div> </aside>');
+document.write('<footer><h3>Footer</h3></footer>');
